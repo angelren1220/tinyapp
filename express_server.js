2f78c58e1012d2235ea1b4571e91f8a4a3741385
Linuxe <angelren1220@gmail.com>
2023-03-20 16:02:10 -0400

    fixed a bug to show longurl

--- express_server.js
+++ express_server.js
@@ -27,8 +27,12 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: req.params[req.params.id] };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 // app.get("/set", (req, res) => {
