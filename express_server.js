1ed0ec3ffd115ff2bd9d61c685500d2c7debdb57
Linuxe <angelren1220@gmail.com>
2023-03-20 14:38:41 -0400

    urls_index.ejs

--- express_server.js
+++ express_server.js
@@ -21,11 +21,16 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 // app.get("/set", (req, res) => {
 //   const a = 1;
 //   res.send(`a = ${a}`);
 //  });
- 
+
 // app.get("/fetch", (req, res) => {
 //   res.send(`a = ${a}`);
 //  });
