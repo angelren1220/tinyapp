3c9f7b62172fb2ede5f73e5bca180c8e4c4f39e1
Linuxe <angelren1220@gmail.com>
2023-03-22 11:31:26 -0400

    refactored generateRandomString function

--- express_server.js
+++ express_server.js
@@ -30,37 +30,8 @@ const users = {
 
 /** functions */
 // generate a string of n random alphnumeric characters
-// a-Z: 65-122; 0-9: 48-57
-// helper: generate a random integer between input min and max
-const getRandomInt = function(min, max) {
-  max += 1;
-  return Math.floor(Math.random() * (max - min) + min);
-};
 const generateRandomString = function(strLen) {
-  let randomString = "";
-  for (let i = 0; i < strLen; i++) {
-    const numOrAlph = getRandomInt(0, 1);
-    // char is number
-    if (numOrAlph) {
-      const charCode = getRandomInt(48, 57);
-      const char = String.fromCharCode(charCode);
-      randomString += char;
-      continue;
-    }
-    // char is alphabet
-    const lowerCaseOrUpperCase = getRandomInt(0, 1);
-    // lowercase
-    if (lowerCaseOrUpperCase) {
-      const charCode = getRandomInt(97, 122);
-      const char = String.fromCharCode(charCode);
-      randomString += char;
-      continue;
-    }
-    // uppercase
-    const charCode = getRandomInt(65, 90);
-    const char = String.fromCharCode(charCode);
-    randomString += char;
-  }
+  let randomString = Math.random().toString(36).substring(2, strLen + 2);
   return randomString;
 };
 
